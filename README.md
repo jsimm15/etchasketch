ed2efcb22f562a8159a748f77111abb1d98b14b9
jsimm15 <justin.simmons.mba@gmail.com>
2022-09-01 12:50:22 -0600

    Add additional functionality to app
    At this point, both the number of grid elements and the coloring
    function applied to the grid have been made dynamic based on user
    input through the use of sidebar buttons. The app begins by generating
    a 50X50 grid, with the black marker function selected. The coloring
    function can be changed to a random color or to white, simulating an
    eraser. At this time, the triggering event for grid coloring has been
    left as a mouseover event, both to simulate an original etch-a-sketch,
    and to keep the code simple. In the future, this may be modified to
    accept click-and-drag in order to offer better user control over the
    placement of activated grid elements.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # etchasketch
 This web app simulates a version of an Etch-a-Sketch by using Javascript
  to manipulate a grid of HTML elements, altering the color of each grid 
- element as the user moves the mouse across the grid. The app will also 
- accept user input to determine the number of elements displayed in the grid.
+ element as the user moves the mouse across the grid. Each pass through 
+ a grid element will update the color of the element, although this is
+ not apparent when using the black marker. 
+ 
+ On the left side of the screen is a sidebar of control butttons that 
+ allow the user to change the coloring function of the app, change the 
+ number of grid elements, and clear the grid back to white.
--- sketch.js
+++ sketch.js
@@ -1,24 +1,27 @@
-function createGrid() {
-    const gridContainer = document.querySelector('.gridContainer');
-    const gridSize = 100;
-    const root = Math.sqrt(gridSize);
-    console.log(root);    
-
+//Create a square grid og size (length X length) elements
+function createGrid(length) {
+    const gridContainer = document.querySelector('.gridContainer');   
+    const gridSize = (length**2);
 
     for (let i=0; i < gridSize; i++){
         let element = document.createElement('div');
         element.id = `div${i+1}`;
-        element.innerText = element.id;
+        element.className = 'gridItem';
+        /*element.innerText = element.id; <--- Toggle to show element number*/
         gridContainer.appendChild(element);
-        element.addEventListener('mouseover', (e) => colorRandom(element));
+        element.addEventListener('mouseover', (e) => colorBlack(element));
     }
-    console.log('before');
-    gridContainer.style.cssText += `grid-template-columns: repeat(${root}, 1fr);`;
-    console.log('after');
+    gridContainer.style.cssText += `grid-template-columns: repeat(${length}, 1fr);`;
 }
+//Paints activated element to black
 function colorBlack(event){
     event.style.backgroundColor ='black';
 }
+//Paints activated element to white
+function colorWhite(event){
+    event.style.backgroundColor = 'white';
+}
+//Helper to select random (r,g,b) color value
 function randomColor(){
     let color = [];
     for (i=0; i<3; i++){ 
@@ -26,11 +29,111 @@ function randomColor(){
     }
     return `rgb(${color[0]}, ${color[1]}, ${color[2]})`
 }
+//Paints activated element to random color
 function colorRandom(event){
-    console.log('selecting...')
     let color = randomColor();
-    console.log(`${color} selected...`)
-
     event.style.backgroundColor = color;
 }
-createGrid();
+//Delete all grid items. To be called before generating new grid
+function deleteContainerItems(){
+    const gridContainer =document.querySelector('.gridContainer');
+    let gridItems = Array.from(gridContainer.childNodes);
+    gridItems.forEach((item) => item.remove());
+}
+//Helper to initialize or reset event listeners
+function resetEventListeners() {
+    const blackButton = document.getElementById('black');
+    const rainbowButton = document.getElementById('rainbow');
+    const clearButton = document.getElementById('clear');
+    const eraserButton =document.getElementById('eraser')
+    const gridContainer =document.querySelector('.gridContainer');
+    let gridItems = Array.from(gridContainer.childNodes);
+
+
+    blackButton.addEventListener('click', () => {
+        gridItems.forEach((item) => {
+            item.addEventListener('mouseover', (e) => colorBlack(item));  
+        })});
+    rainbowButton.addEventListener('click', () => {
+        gridItems.forEach((item) => {
+            item.addEventListener('mouseover', (e) => colorRandom(item));
+        })});
+    clearButton.addEventListener('click', () =>{
+        gridItems.forEach((item) => {
+            item.style.backgroundColor = 'white';
+        })});
+    eraserButton.addEventListener('click', () => {
+        gridItems.forEach((item) => {
+            item.addEventListener('mouseover', (e) => colorWhite(item));
+        })});
+
+}
+//Builds sidebar of functional buttons on left of screen
+function createSidebar(){
+    const sb = document.querySelector('.sidebar');
+    const items = document.querySelectorAll('.gridItem');
+    itemsArr = Array.from(items);
+
+    //Create button to change to 10X10 grid
+    const gridTenButton = document.createElement('button');
+    gridTenButton.id = 'ten';
+    gridTenButton.innerText = '10 X 10';
+    sb.appendChild(gridTenButton);
+    gridTenButton.addEventListener('click', () => {
+        deleteContainerItems();
+        createGrid(10);
+        resetEventListeners();
+    });
+    //Create button to change to 50X50 grid
+    const grid50button = document.createElement('button');
+    grid50button.id = 'fifty';
+    grid50button.innerText = '50 X 50';
+    sb.appendChild(grid50button);
+    grid50button.addEventListener('click', () => {
+        deleteContainerItems();
+        createGrid(50);
+        resetEventListeners();
+    });
+    //Create button to change to 100 X 100 grid
+    const grid100Button = document.createElement('button');
+    grid100Button.id = 'hundred';
+    grid100Button.innerText = '100 X 100';
+    sb.appendChild(grid100Button);
+    grid100Button.addEventListener('click', () => {
+        deleteContainerItems();
+        createGrid(100);
+        resetEventListeners();
+    })
+    //Create button to enable black coloring
+    const blackButton = document.createElement('button');
+    blackButton.id = 'black';
+    blackButton.innerText = 'Black Marker';
+    sb.appendChild(blackButton);
+        
+    //Create button to enable random coloring
+    const rainbowButton =document.createElement('button');
+    rainbowButton.id = 'rainbow';
+    rainbowButton.innerText = 'Rainbow';
+    sb.appendChild(rainbowButton);
+
+    //Create eraser button to change individual elements to white
+    const eraserButton = document.createElement('button');
+    eraserButton.id = 'eraser';
+    eraserButton.innerText = 'Eraser';
+    sb.appendChild(eraserButton);
+    
+    //Create button to clear grid back to all white
+    const clearButton = document.createElement('button');
+    clearButton.id = 'clear';
+    clearButton.innerText = 'Clear';
+    sb.appendChild(clearButton);
+    
+    
+    //Initialize button event listeners
+    resetEventListeners();
+}
+
+//Initialize grid
+createGrid(50);
+//Initialize sidebar
+createSidebar();
